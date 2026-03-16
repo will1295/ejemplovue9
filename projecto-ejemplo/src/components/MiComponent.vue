f96efc1fd0665afeb62fd8c0ebb7e1aef12fc880
Willian <willian120795@gmail.com>
2026-03-16 12:11:34 -0600

    cambios al repo

---
+++ projecto-ejemplo/src/components/MiComponent.vue
@@ -0,0 +1,13 @@
+<script setup>
+    import {ref} from 'vue';
+    let texto = ref("")
+</script>
+
+<template>
+    <input v-model="texto" type="text" placeholder="Escribe algo">
+    <label for="">{{ texto }}</label>
+
+</template>
+
+<style scoped>
+</style>
